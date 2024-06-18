6610596dbd6695465e2d33e10f057b5552ee19db
Dennis Aldana <dennis.aldana@gmail.com>
2024-06-17 18:38:19 -0600

    add: map loading and rendering

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,74 @@
-
 use minifb::{Key, Window, WindowOptions};
 use std::time::Duration;
+use std::fs::File;
+use std::io::{BufRead, BufReader};
 mod framebuffer;
+use framebuffer::Framebuffer;
+
+fn load_maze(filename: &str) -> Vec<Vec<char>> {
+    let file = File::open(filename).unwrap();
+    let reader = BufReader::new(file);
+
+    reader
+        .lines()
+        .map(|line| line.unwrap().chars().collect())
+        .collect()
+}
+
+fn draw_cell(framebuffer: &mut Framebuffer, x: usize, y: usize, block_size: usize, cell: char) {
+  if cell == ' ' {
+    return;
+  }
+
+  for x in x..x + block_size {
+    for y in y..y + block_size {
+      match cell {
+        '+' | '-' | '|' => {
+          framebuffer.set_current_color(0xFFDDDD);
+          framebuffer.point(x, y);
+        },
+        _ => (),
+      }
+    }
+  } 
+}
+
+fn render(framebuffer: &mut Framebuffer) {
+  let maze = load_maze("./maze.txt");
+  let block_size = 100;  // 10 pixels each block
+
+  for row in 0..maze.len() {
+    for col in 0..maze[row].len() {
+      let x = col * block_size;
+      let y = row * block_size;
+      draw_cell(framebuffer, x, y, block_size, maze[row][col]);
+    }
+  } 
+}
 
 fn main() {
-  let window_width = 800;
-  let window_height = 600;
+  let window_width = 1300;
+  let window_height = 900;
 
-  let framebuffer_width = 80;
-  let framebuffer_height = 60;
+  let framebuffer_width = 1300;
+  let framebuffer_height = 900;
 
   let frame_delay = Duration::from_millis(16);
 
   let mut framebuffer = framebuffer::Framebuffer::new(framebuffer_width, framebuffer_height);
 
   let mut window = Window::new(
-    "Rust Graphics - Framebuffer Example",
+    "Rust Graphics - Maze Example",
     window_width,
     window_height,
     WindowOptions::default(),
   ).unwrap();
 
-  framebuffer.set_background_color(0x333355);
+  // move the window around
+  window.set_position(100, 100);
+  window.update();
 
-  let mut x = 1 as i32;
-  let mut speed = 1 as i32;
+  framebuffer.set_background_color(0x333355);
 
   while window.is_open() {
     // listen to inputs
@@ -32,21 +76,11 @@ fn main() {
       break;
     }
 
-    // prepare variables for rendering
-    if x as usize == framebuffer_width {
-      speed = -1;
-    }
-    if x == 0 {
-      speed = 1;
-    }
-    x += speed;
-
     // Clear the framebuffer
     framebuffer.clear();
 
-    // Draw some points
-    framebuffer.set_current_color(0xFFDDDD);
-    framebuffer.point(x as usize, 40);
+    // Draw some stuff
+    render(&mut framebuffer);
 
     // Update the window with the framebuffer contents
     window
