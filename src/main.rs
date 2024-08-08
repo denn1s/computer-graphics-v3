e40573d4ade8c132ec36cd81a9c9dc936f9e31e6
Dennis Aldana <dennis.aldana@gmail.com>
2024-08-07 20:12:59 -0600

    fix: add perspective correction

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use nalgebra_glm::{Vec3, normalize};
 use minifb::{Key, Window, WindowOptions};
 use std::time::Duration;
+use std::f32::consts::PI;
 
 mod framebuffer;
 mod ray_intersect;
@@ -25,6 +26,8 @@ pub fn render(framebuffer: &mut Framebuffer, objects: &[Sphere]) {
     let width = framebuffer.width as f32;
     let height = framebuffer.height as f32;
     let aspect_ratio = width / height;
+    let fov = PI/3.0;
+    let perspective_scale = (fov * 0.5).tan();
 
     for y in 0..framebuffer.height {
         for x in 0..framebuffer.width {
@@ -32,8 +35,9 @@ pub fn render(framebuffer: &mut Framebuffer, objects: &[Sphere]) {
             let screen_x = (2.0 * x as f32) / width - 1.0;
             let screen_y = -(2.0 * y as f32) / height + 1.0;
 
-            // Adjust for aspect ratio
-            let screen_x = screen_x * aspect_ratio;
+            // Adjust for aspect ratio and perspective 
+            let screen_x = screen_x * aspect_ratio * perspective_scale;
+            let screen_y = screen_y * perspective_scale;
 
             // Calculate the direction of the ray for this pixel
             let ray_direction = normalize(&Vec3::new(screen_x, screen_y, -1.0));
