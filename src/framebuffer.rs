6cf2b40b370885e3972e69dbd4275c6dd3826470
Dennis Aldana <dennis.aldana@gmail.com>
2025-06-26 01:35:50 -0600

    fix: remove unnecesary clear

--- src/framebuffer.rs
+++ src/framebuffer.rs
@@ -47,14 +47,11 @@ impl Framebuffer {
         window: &mut RaylibHandle,
         raylib_thread: &RaylibThread,
     ) {
-        // the window currently has the "old" data (previous frame)
-        
         // we get the "new" data from the new buffer into texture
         if let Ok(texture) = window.load_texture_from_image(raylib_thread, &self.color_buffer) {
-            let mut renderer = window.begin_drawing(raylib_thread);
 
-            // we clear the "old" data from the window, this effectively erases the previous frame
-            renderer.clear_background(self.background_color);
+            // the window currently has the "old" data (previous frame)
+            let mut renderer = window.begin_drawing(raylib_thread);
 
             // we move the "new" data to the window (current frame) 
             renderer.draw_texture(&texture, 0, 0, Color::WHITE);
